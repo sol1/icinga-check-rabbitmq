c24c57206e60f6ed134797c4357bd13e2be94ea6
Andrew Foster <andy@sol1.com.au>
2016-01-23 17:35:10 +1100

    First draft of check_rabbitmq_consumers.sh

--- README.md
+++ README.md
@@ -4,24 +4,26 @@ Some lightweight Nagios/Icinga check plugins for RabbitMQ.
 
 ## Status
 
-Unreleased; not yet suitable for any use.
+Alpha, in testing, not suitable for production use.
 
 ## Checks
 
-The following types of checks are currently supported:
+The following checks are included:
 
- * None
+ * `check_rabbitmq_consumers.sh`: Number of consumers on a queue
 
 ## Requirements
 
 The basic check logic is written in bash. Checks rely on the HTTP API provided
-by the RabbitMQ management interface and also depend on having the following
-utilities in your `$PATH`:
+by the RabbitMQ management interface, so the `rabbitmq-management` plugin must
+be enabled and the API accessible from the machine you intend to run the check
+from (probably localhost).
+
+You also need the following utilities in your `$PATH`:
 
  * [curl](http://curl.haxx.se/)
  * [jq](https://stedolan.github.io/jq/)
 
 ## TODO
 
- * Number of consumers on a queue
  * TODO
---
+++ check_rabbitmq_consumers.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# 
+# check_rabbitmq_consumers.sh
+# https://github.com/sol1/icinga-check-rabbitmq
+# 
+# Standard Icinga return codes
+E_SUCCESS="0"
+E_WARNING="1"
+E_CRITICAL="2"
+E_UNKNOWN="3"
+
+usage() {
+	cat << EOU
+Usage: $0 <OPTIONS>
+	-u <RabbitMQ API username>
+	-p <RabbitMQ API password> 
+	-a <API URL, e.g. http://localhost:15672/api/consumers>
+	-q <RabbitMQ queue name, e.g. remote_deliveries>
+	-c <Number of consumers when CRITICAL or less>
+	-w <Number of consumers when WARNING or less>
+
+Example:
+	$0 -u guest -p guest -a "http://localhost:15672/api/consumers" -q remote_deliveries -c 1 -w 2
+EOU
+	exit ${E_UNKNOWN}
+}
+
+while getopts ":u:p:a:q:c:w:" o; do
+    case "${o}" in
+        u)
+            u=${OPTARG}
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        a)
+            a=${OPTARG}
+            ;;
+        q)
+            q=${OPTARG}
+            ;;
+        c)
+            c=${OPTARG}
+            ;;
+        w)
+            w=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${u}" ] || [ -z "${p}" ] || [ -z "${a}" ] || [ -z "${q}" ] || [ -z "${c}" ] || [ -z "${w}" ]; then
+	usage
+fi
+
+if ! [ -x `which curl` ]; then
+	echo "UNKNOWN: curl not found"
+	exit ${E_UNKNOWN}
+fi
+
+if ! [ -x `which jq` ]; then
+	echo "UNKNOWN: jq not found"
+	exit ${E_UNKNOWN}
+fi
+
+response=`curl -s -u $u:$p -o - "$a"`
+
+# Check for a simple error provided by the API
+api_error=`echo $response | jq ".error?"`
+
+if ! [ -z "${api_error}" ]; then
+	echo "CRITICAL: API returned: ${api_error}"
+	exit ${E_CRITICAL}
+fi
+
+if [ $? != 0 ]; then
+	echo "CRITICAL: Bad response from RabbitMQ HTTP API"
+	exit ${E_CRITICAL}
+else
+	consumers=`echo $response | jq "map(select(.queue.name == \"$q\")) | length"`
+fi
+
+if [ -z "${consumers}" ]; then
+	echo "CRITICAL: No response from RabbitMQ HTTP API"
+	exit ${E_CRITICAL}
+fi
+
+if [ $consumers -gt $w ]; then
+	echo "OK: ${q} has ${consumers} consumers"
+	exit ${E_SUCCESS}
+fi
+
+if [ $consumers -le $c ]; then
+	echo "CRITICAL: ${q} has ${consumers} consumers"
+	exit ${E_CRITICAL}
+fi
+
+if [ $consumers -le $w ]; then
+	echo "WARNING: ${q} has ${consumers} consumers"
+	exit ${E_WARNING}
+fi
+
+exit ${E_UNKNOWN}
